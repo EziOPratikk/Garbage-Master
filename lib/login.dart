5f545589dc252a6188c7e0a1b69649a3dfc440f9
Pratik <khadkapratikk10@gmail.com>
2022-12-08 15:28:50 +0545

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _header(context),
+            _inputField(context),
+          ],
+        ),
+      )),
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Text(
+          'LOGO',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text('Welcome to Garbage Master'),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Email",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.4),
+            filled: true,
+            prefixIcon: Icon(Icons.person),
+          ),
+        ),
+        SizedBox(height: 20),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.4),
+            filled: true,
+            prefixIcon: Icon(Icons.person),
+          ),
+          obscureText: true,
+        ),
+        TextButton(onPressed: () {}, child: Text('Forgot Password')),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            'Login',
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ButtonStyle(
+            shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15))),
+            padding:
+                MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 15)),
+          ),
+        ),
+        Row(
+          children: [
+            Text('Don\'t have an account?'),
+            TextButton(onPressed: () {}, child: Text('Register')),
+          ],
+        )
+      ],
+    );
+  }
+}
